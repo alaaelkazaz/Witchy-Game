39cae10664e553ad2b47d7c323e5feb6f5039fdf
Alaa Elkzaz <52638420+leoelkzaz@users.noreply.github.com>
2020-04-29 19:56:20 +0200

    Update score.py

--- score.py
+++ score.py
@@ -1,38 +1,50 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-import pygame
 import random
 
+
+
+score1 = 17
 def init():
-	glClearColor(0,0,0,0)
+	glClearColor(1,1,1,1)
 	glMatrixMode(GL_PROJECTION)
 	glLoadIdentity()
 	glOrtho(0,1,0,1,0,1)
 	glMatrixMode(GL_MODELVIEW)
 
 
-def display_score():
-   glLineWidth(5)
-    glColor(0,0,1,1)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluOrtho2D(0,1000,0,1000)
-    glMatrixMode(GL_MODELVIEW)
-    string = "Hello Alaa"
-    string = string.encode()
-    for x in string:
-        glutStrokeCharacter(GLUT_STROKE_ROMAN, x)
-    glutSwapBuffers()
+class score_Dispaly():
+    global score1 
+    def score_Panel():
 
+     glClearColor(1,1,1,1)
+     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+     glLineWidth(3)
+     glColor(1,0,0,1)
+
+     glMatrixMode(GL_PROJECTION)
+     glLoadIdentity()
+
+     gluOrtho2D(0,1500,0,1500)
+     glMatrixMode(GL_MODELVIEW)
+     glLoadIdentity()
+     scoreLabel="Your Score : "
+     scoreString=str(score1)
+     scorPara=scoreLabel + scoreString
+     scorPara= scorPara.encode()
+     for x in scorPara:
+         glutStrokeCharacter(GLUT_STROKE_ROMAN, x)
+    
+     glutSwapBuffers()
 
 glutInit()
 glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE)
 glutInitWindowSize(500,500)
 glutCreateWindow(b'Only The Score Feature')
-glutDisplayFunc(display_score)
-glutIdleFunc(display_score)
+glutDisplayFunc(score_Dispaly.score_Panel)
+glutIdleFunc(score_Dispaly.score_Panel)
 init()
 glutMainLoop()
-
 
